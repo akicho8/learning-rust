4451f385a718ce845dee535e18b6bdc4b82f3f2e
akicho8 <akicho8@gmail.com>
2021-07-03 14:15:18 +0900

    [feat] add clone len ..

--- src/test_basic.rs
+++ src/test_basic.rs
@@ -30,6 +30,39 @@ mod tests {
         println!("{}", v);
         let v: f64 = 10.0 / 3.0;
         println!("{}", v);
+    }
+    #[test]
+    fn test_array() {
+        assert_eq!("b", ["a", "b", "c"][1]);
+    }
+    #[test]
+    fn test_let() {
+        let v2 = 1;
+        let v1 = {
+            let v2 = 100;
+            v2 + 23            // 最後に ; をつけると謎のエラーがでる
+        };
+        assert_eq!(123, v1);
+        assert_eq!(1, v2);
+    }
+    #[test]
+    fn test_if() {
+        let v1 = if true {
+            1
+        } else {                // = if の場合は else がないとエラーになる！？
+            2
+        };
+        assert_eq!(1, v1);
 
+        if true {
+            let v1 = 100;
+            assert_eq!(100, v1);
+        }
+    }
+    #[test]
+    fn test_iter() {
+        for e in ["a", "b"].iter() {
+            println!("--> {}", e);
+        }
     }
 }
--- src/test_string.rs
+++ src/test_string.rs
@@ -1,3 +1,4 @@
+// -*- compile-command: "cargo test -- --nocapture" -*-
 // pub fn my_func2(a: i32, b: i32) -> i32 {
 //     a + b
 // }
@@ -8,5 +9,43 @@ mod tests {
     #[test]
     fn test_string_parse() {
         assert_eq!(123, "123".parse().expect("expect_message"));
+    }
+    #[test]
+    fn test_string_from() {
+        let mut s1 = String::from("foo"); // let mut s1 = "foo"; と書いた場合は push_str できない。なんで？
+        s1.push_str("bar");
+        assert_eq!("foobar", s1);
+
+        // let s1 = String::from("foo");
+        // let s2 = s1; // s1 を s2 にコピーしたあと s1 を使うとエラーになる
+        // println!("s2 -> {}", s1);
+    }
+    #[test]
+    fn test_string_clone() {
+        let s1 = String::from("foo");
+        let s2 = s1.clone();    // 実体をコピーしている
+        assert_eq!(s1, s2);
+    }
+    #[test]
+    fn test_string_len() {
+        assert_eq!(3, "foo".len());
+        assert_eq!(3, String::from("foo").len());
+    }
+    #[test]
+    fn test_string_as_bytes() {
+        // let s1 = String::from("foo");
+        // assert_eq!(3, s1.as_bytes());
+    }
+    #[test]
+    fn test_string_clear() {
+        let mut s1 = String::from("foo");
+        s1.clear();
+        assert_eq!("", s1);
+    }
+    #[test]
+    fn test_string_slice() {
+        let s1 = "foobar";
+        assert_eq!("fo", &s1[0..2]); // .. は ... と考えて 0...2 が ruby の 0..1 になる
+        assert_eq!("ba", &s1[3..5]);
     }
 }
